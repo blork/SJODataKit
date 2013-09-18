b110307d2fde5f93c475f4a9aa4824f89db5acbb
Sam Oakley <sam@blork.co.uk>
2013-09-18 21:30:01 +0100

    Removed back ticks, as Xcode 5 doesn't parse them.

--- Library/SJOSearchableFetchedResultsController.h
+++ Library/SJOSearchableFetchedResultsController.h
@@ -16,13 +16,13 @@ This class provides a simpler way to replicate the often-used pattern of a searc
 @interface SJOSearchableFetchedResultsController : UITableViewController<UISearchBarDelegate, UISearchDisplayDelegate, NSFetchedResultsControllerDelegate>
 
 /**
- The `SJODataStore` to be used when querying data.
+ The SJODataStore to be used when querying data.
  @warning This must be set before the view is loaded.
  */
 @property (strong, nonatomic) SJODataStore *store;
 
 /**
- The `UISearchDisplayController` used to manage the search interface.
+ The UISearchDisplayController used to manage the search interface.
  @discussion You can customise it in your subclass to enable scope buttons, etc.
  */
 @property (strong, nonatomic, readonly) UISearchDisplayController *searchController;
@@ -30,22 +30,22 @@ This class provides a simpler way to replicate the often-used pattern of a searc
 @property (strong, nonatomic) UIView *emptyView;
 
 /**
- Returns the currently active `UITableView` (i.e. regular or search).
- @return The `UITableView` that is currently active.
+ Returns the currently active UITableView (i.e. regular or search).
+ @return The UITableView that is currently active.
  */
 -(UITableView*)activeTableView;
 
 /**
- Returns the currently active `NSFetchedResultsController` (i.e. regular or search).
- @return The `NSFetchedResultsController` that is currently active.
+ Returns the currently active NSFetchedResultsController (i.e. regular or search).
+ @return The NSFetchedResultsController that is currently active.
  */
 - (NSFetchedResultsController *)activeFetchedResultsController;
 
 
 /**
- Returns the appropiate `NSFetchedResultsController` (i.e. regular or search) for the given `UITableView`.
- @param tableView The `UITableView` you wish to retrieve the `NSFetchedResultsController` for.
- @return The `NSFetchedResultsController` that is managing the given `UITableView`.
+ Returns the appropiate NSFetchedResultsController (i.e. regular or search) for the given UITableView.
+ @param tableView The UITableView you wish to retrieve the NSFetchedResultsController for.
+ @return The NSFetchedResultsController that is managing the given UITableView.
  */
 - (NSFetchedResultsController *)fetchedResultsControllerForTableView:(UITableView *)tableView;
 
@@ -63,10 +63,10 @@ This class provides a simpler way to replicate the often-used pattern of a searc
 - (void)fetchedResultsController:(NSFetchedResultsController *)fetchedResultsController configureCell:(UITableViewCell *)cell atIndexPath:(NSIndexPath *)indexPath;
 
 /**
- Returns a new `NSFetchRequest` for the specified search string.
- @discussion Override this method in your subclass to return the appropriate `NSFetchRequest` for the search term. If `searchString` is `nil`, return your unfiltered dataset.
+ Returns a new NSFetchRequest for the specified search string.
+ @discussion Override this method in your subclass to return the appropriate NSFetchRequest for the search term. If searchString is nil, return your unfiltered dataset.
  @param searchString The query entered by a user. May be nil.
- @return The `NSFetchRequest` to be executed by the `NSFetchedResultsController`.
+ @return The NSFetchRequest to be executed by the NSFetchedResultsController.
  @warning This method must be overidden in your subclass.
  */
 - (NSFetchRequest *)fetchRequestForSearch:(NSString *)searchString;
